4a5dbc29319c4b5935725750743cebd18619d283
santhsatsangi <gosharavind05@gmail.com>
2022-02-22 14:08:54 +0530

    bootstrap config

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
-const OTP = Math.round(Math.random()*100000);
+ const OTP = Math.round(Math.random()*100000);
 let p = document.createElement("div");
 p.id = OTP.toString();
 document.body.appendChild(p);
 p.innerHTML = OTP.toString()
 p.style.fontSize="60px";
-
-
-
